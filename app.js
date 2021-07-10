8079da9e5a5bb93754d094e11a952467c7aaae31
Guillaume LE BOLLOCH <lebolloch.g@gmail.com>
2021-07-10 07:44:09 +0200

    side bar modif + lighthouse

--- app.js
+++ app.js
@@ -8,4 +8,15 @@
     e.preventDefault();
     $("body").removeClass("with-sidebar");
   });
+
+  $("a[href*='#']:not([href='#'])").click(function () {
+    {
+      var anchor = $(this.hash);
+      anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) + "]");
+      if (anchor.length) {
+        $("body").removeClass("with-sidebar");
+        $("html, body").animate({ scrollTop: anchor.offset().top }, 200);
+      }
+    }
+  });
 })(jQuery);
